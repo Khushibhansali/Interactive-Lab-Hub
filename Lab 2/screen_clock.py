a5b272ec2920cc78ecb43856c2658947b76e4ab3
Khushibhansali <30347957+Khushibhansali@users.noreply.github.com>
2023-09-16 01:23:22 -0400

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -5,7 +5,6 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 
-
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
@@ -61,41 +60,99 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
-# Define different scenes and fish for different times of day
-scenes = {
-    "morning": "./Images/morning.jpeg",
-    "afternoon": "./Images/afternoon.jpeg",
-    "night": "./Images/night.jpeg",
-}
+dinosaur = Image.open("dinosaur.png")  # Replace with the path to your dinosaur image
+dinosaur_width, dinosaur_height = 20, 40  # Set the size of the dinosaur image
+dinosaur = dinosaur.resize((dinosaur_width, dinosaur_height))
+dinosaur_x = 0
+dinosaur_y = height // 2
+dinosaur_updated = False  # Track if the dinosaur was updated in this frame
+
+# Set up button input
+button_pin = digitalio.DigitalInOut(board.D23)
+button_pin.switch_to_input(pull=digitalio.Pull.UP)
+
+# Load the cactus image
+cactus = Image.open("cactus.png")  # Replace with the path to your cactus image
+cactus_width, cactus_height = 20, 40  # Set the size of the cactus image
+cactus = cactus.resize((cactus_width, cactus_height))
+
+# Set the initial position of the dot and the cactus
+dot_x = 0
+dot_y = height // 2
+dot_updated = False  # Track if the dot was updated in this frame
+
+cactus_x = width - cactus_width
+cactus_y = height // 2
+
+buffer = Image.new("RGB", (width, height), (0, 0, 0))
+buffer_draw = ImageDraw.Draw(buffer)
+
+background = Image.open("background.jpeg")  # Replace with the path to your background image
+background = background.resize((width, height))
+
 
 while True:
-    current_time = time.strftime("%H:%M")
+    # Check for button press to move the dot (ball) up
+    if not button_pin.value:
+        dot_y -= 10  # Adjust the speed by changing the value
 
-    # Determine the time of day based on the current hour
-    current_hour = int(time.strftime("%H"))
-    if 6 <= current_hour < 12:
-        time_of_day = "morning"
-    elif 12 <= current_hour < 18:
-        time_of_day = "afternoon"
-    else:
-        time_of_day = "night"
+    # Clear the buffer image.
+    # buffer_draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    buffer_draw.rectangle((dinosaur_x, dinosaur_y, dinosaur_x + dinosaur_width, dinosaur_y + dinosaur_height), outline=0, fill=(0, 0, 0))
 
-    # Load the appropriate scenery and fish image based on the time of day
-    scenery_and_fish_image = Image.open(scenes[time_of_day])
+    #buffer_draw.rectangle((dot_x, height // 2 - 2, dot_x + 4, height // 2 + 2), outline=0, fill=(0, 0, 0))
 
-    # Resize the image to match the display's dimensions (135x240)
-    scenery_and_fish_image = scenery_and_fish_image.resize((135, 240))
+    # Paste the background image on the buffer
+    buffer.paste(background, (0, 0))
 
-    # Create an image to overlay the text on
-    image_with_text = scenery_and_fish_image.copy()
-    draw = ImageDraw.Draw(image_with_text)
+    # Update the position of the dot if necessary
+    if dot_updated:
+        dot_x += 1  # Adjust the speed by changing the value
+        dot_updated = False
 
-    # Draw the current time on the overlay
-    draw.text((5, 5), "Time:", font=font, fill=(255, 255, 255))
-    draw.text((10, 40), current_time, font=font, fill=(255, 255, 255))
+    # Ensure the dot stays within the screen width
+    if dot_x >= width:
+        dot_x = 0  # Reset the dot to the left edge
 
-    # Display the image with the overlay
-    disp.image(image_with_text)
+    if dinosaur_x >= width:
+        dinosaur_x = 0  # Reset the dinosaur to the left edge
 
-    # Update the display every minute
-    time.sleep(60)
+    # Move the cactus towards the dot
+    cactus_x -= 1  # Adjust the speed by changing the value
+
+    # Reset the cactus position when it goes off-screen
+    if cactus_x < -cactus_width:
+        cactus_x = width
+
+    # Check for collision between the dot and the cactus
+    if (
+        dot_x < cactus_x + cactus_width
+        and dot_x + 4 > cactus_x
+        and height // 2 + 2 > cactus_y
+        and height // 2 - 2 < cactus_y + cactus_height
+    ):
+        # Collision occurred, reset the dot to its original position
+        dot_x = 0
+
+    # Draw the dot at the updated position on the buffer
+    buffer_draw.rectangle((dot_x, height // 2 - 2, dot_x + 4, height // 2 + 2), fill=(255, 255, 255))
+
+    # Check for collision between the dinosaur and the cactus
+    if (
+        dinosaur_x < cactus_x + cactus_width
+        and dinosaur_x + dinosaur_width > cactus_x
+        and dinosaur_y + dinosaur_height > cactus_y
+        and dinosaur_y < cactus_y + cactus_height
+    ):
+        # Collision occurred, reset the dinosaur to its original position
+        dinosaur_x = 0
+
+    # Draw the dinosaur at the updated position on the buffer
+    buffer.paste(dinosaur, (dinosaur_x, dinosaur_y))
+
+    # Paste the cactus image on the buffer
+    buffer.paste(cactus, (cactus_x, cactus_y))
+
+    # Display the buffer image on the screen.
+    disp.image(buffer, rotation)
+    time.sleep(0.05)  # Adjust the delay as needed

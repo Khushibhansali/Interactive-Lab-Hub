9f4fa808410a617d5eccc49b90cf4dea723ad928
William J. Reid <wjr83@cornell.edu>
2023-10-02 23:07:49 -0400

    Minor updates

--- Lab 3/speech-scripts/music_player.sh
+++ Lab 3/speech-scripts/music_player.sh
@@ -5,16 +5,16 @@ say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "
 #say $*
 say " Welcome to the Master Music Player, where you can transform any object into a musical instrument!"
 say " Please listen carefully to the instructions:"
-say "Say the keyword beats to press the individual touch sensors and play sounds. "
+say "Say the keyword, beats, to press the individual touch sensors and play sounds. "
 say " Sensor number one is empty on the touch sensor to allow you to record a sound. All other buttons are pre-recorded sounds. "
 say " You can switch instruments by pressing 0 on the touch sensor and then saying guitar or beats"
-say " Say the keyword guitar to play guitar notes on the individual touch sensors."
+say " Say the keyword, guitar, to play guitar notes on the individual touch sensors."
 # say " Say the keyword piano to play piano notes on the individual touch sensors."
 say "Say the keyword record and then press the green button to record a sound, "
 # say "Say volume to press keyboard button and adjust volume with *, "
 # say "Say tempo to press keyboard and have tempo adjusted by pressing numbers on the keypad followed by #, "
-say "Say music to layer songs and press 3 sounds to layer on the touch sensors. "
-say "Say instructions to hear the instructions again."
+say "Say the keyword, music, to layer songs and press 3 sounds to layer on the touch sensors. "
+say "Say the keyword, instructions, to hear the instructions again."
 
 # speak_instructions() {
 #         espeak -ven+f2 -k5 -s150 --stdout "$(cat instructions.txt)" | aplay
